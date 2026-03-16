a2348c8e892ab0d328703b423879d53285f5ec46
VictorNikolov <VBNikolov23@codingburgas.bg>
2026-03-16 15:52:26 +0200

    Improve Main Menu

--- project/Library/lib.h
+++ project/Library/lib.h
@@ -1,4 +1,6 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include "string"
 int f(int, int);
+void Menu();
--- project/main/main.cpp
+++ project/main/main.cpp
@@ -1,9 +1,15 @@
 #include "../Library/lib.h"
+void Menu()
+{
+    std::ifstream file("../menu.txt");
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+}
 int main()
 {
-	void f()
-	{
-		std::cout << ifstream("menu/txt").rdbuf();
-	}
-	
+    Menu();
 }
